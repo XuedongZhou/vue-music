b32b1d70b4828aabb79af0fc6d79a4f8d988001f
Xuedong Zhou <you@example.com>
2019-02-01 16:38:57 +0800

    '移动端QqMusic'

---
+++ src/common/js/song.js
@@ -0,0 +1,56 @@
+import {getLyric} from 'api/song'
+import {ERR_OK} from 'api/config'
+import {Base64} from 'js-base64'
+
+export default class Song {
+  constructor({ id, mid, singer, name, album, duration, image, url }) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.url = url
+  }
+
+  getLyric() {
+    if (this.lyric) {
+      return Promise.resolve(this.lyric)
+    }
+    return new Promise((resolve, reject) => {
+      getLyric(this.mid).then((res) => {
+        if (res.retcode === ERR_OK) {
+          this.lyric = Base64.decode(res.lyric)
+          resolve(this.lyric)
+        } else {
+          reject('no lyric')
+        }
+      })
+    })
+  }
+}
+
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+    url: `http://isure.stream.qqmusic.qq.com/C400${musicData.strMediaMid}.m4a?guid=2639762916&vkey=BB9364C0BC7A3A42828D38F647EEE4A542B250B1AE95C6C45FDEA6A944DA68E3F2D9BF81BF21A370EB05D157182C01A80F0B2C39007E2A9E&uin=0&fromtag=38`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
